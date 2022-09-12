ec9ada7735d4846ebe3f919b6b6ab2f39332dc21
efran <efran@bitcoin.co.di>
2022-09-12 17:46:15 +0700

    slicing result page

--- src/components/LayoutComponent.vue
+++ src/components/LayoutComponent.vue
@@ -1,7 +1,23 @@
+<script setup>
+const props = defineProps({
+  isResult: {
+    default: false,
+    type: Boolean,
+  },
+  customBackground: {
+    default: "",
+    type: String,
+  },
+});
+</script>
+
 <template>
   <main class="flex justify-center">
     <div
-      class="w-[340px] bg-gradient-to-b from-[#712bda] to-[#a45deb] h-screen"
+      class="w-96 h-screen"
+      :class="`${
+        props.isResult ? '' : 'bg-gradient-to-b from-[#712bda] to-[#a45deb]'
+      } ${props.customBackground}`"
     >
       <slot />
     </div>
---
+++ src/views/ResultView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import LayoutComponent from "../components/LayoutComponent.vue";
+</script>
+
+<template>
+  <LayoutComponent is-result custom-background="bg-[#f8fafc]">
+    <div
+      class="flex justify-between bg-gradient-to-r from-[#712bda] to-[#a45deb] px-4 py-3 text-white shadow-lg"
+    >
+      <div>menu</div>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="@/assets/ngmusic.svg"
+        width="72"
+        height="15"
+      />
+      <div>menu</div>
+    </div>
+    <div class="mt-10 mb-7 flex flex-col justify-between">
+      <div class="text-center text-sm flex gap-2 items-center justify-center">
+        <div class="">Search Result For:</div>
+        <div class="text-[#7b34dd] font-bold text-lg ml">hasil</div>
+      </div>
+
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </LayoutComponent>
+</template>
